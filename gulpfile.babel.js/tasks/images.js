9699a686358876cf8568788eae137f6c29448121
RomanStashuk <romanstashuk1@gmail.com>
2022-07-11 01:11:50 +0300

    add task for images

---
+++ gulpfile.babel.js/tasks/images.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import gulp from 'gulp';
+
+import path from '../config/paths.js';
+import app from '../config/app.js';
+
+// Plugins
+import loadPlugins from 'gulp-load-plugins';
+const gp = loadPlugins();
+
+import browserSync from 'browser-sync';
+
+// Обрoбка зображень
+export default () => {
+  return gulp.src(path.img.src)
+    .pipe(gp.plumber({
+      errorHandler: gp.notify.onError(error => ({
+        title: 'Images',
+        message: error.message
+      }))
+    }))
+    .pipe(gp.newer(path.img.dest))
+    .pipe(gp.webp())
+    .pipe(gulp.dest(path.img.dest))
+    .pipe(gulp.src(path.img.src))
+    .pipe(gp.newer(path.img.dest))
+    .pipe(gp.if(app.isProd, gp.imagemin(app.imagemin)))
+    .pipe(gulp.dest(path.img.dest))
+    .pipe(browserSync.stream());
+}
